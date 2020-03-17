b734c0e833b207872ffb9f36c19b984acb1d2769
jim <18361277396@163.com>
2020-03-18 06:16:47 +0800

    适配器模式

---
+++ design-pattern/src/main/java/com/design/pattern/study/adapterpattern/Adaptee.java
@@ -0,0 +1,13 @@
+package com.design.pattern.study.adapterpattern;
+
+/**
+ * @author a small asshole
+ * @version 1.0
+ * @description 被适配者
+ * @date 2020/3/17
+ * @since 1.0
+ */
+public class Adaptee {
+    public void fa(){}
+    public void fb(){}
+}
---
+++ design-pattern/src/main/java/com/design/pattern/study/adapterpattern/TargetInter.java
@@ -0,0 +1,13 @@
+package com.design.pattern.study.adapterpattern;
+
+/**
+ * @author a small asshole
+ * @version 1.0
+ * @description 标准api
+ * @date 2020/3/17
+ * @since 1.0
+ */
+public interface TargetInter {
+    public void f1();
+    public void f2();
+}
